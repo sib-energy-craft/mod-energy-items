52e58dd01454eecb20abc8b32d7a2fb5a96a4a64
sibmaks <sibmaks@vk.com>
2023-04-27 19:10:03 +0300

    Add: - advanced_rechargeable_battery - coil - energy_motor - energy_tool_body - mixed_metal_ingot - advanced_alloy

--- src/main/java/com/github/sib_energy_craft/energy_items/load/Items.java
+++ src/main/java/com/github/sib_energy_craft/energy_items/load/Items.java
@@ -14,17 +14,43 @@ import static com.github.sib_energy_craft.sec_utils.utils.ItemUtils.register;
  */
 public final class Items implements ModRegistrar {
     public static final BatteryItem RECHARGEABLE_BATTERY;
+    public static final BatteryItem ADVANCED_RECHARGEABLE_BATTERY;
+
     public static final Item ENERGY_CRYSTAL;
+
     public static final Item ENERGY_CIRCUIT;
     public static final Item ADVANCED_ENERGY_CIRCUIT;
 
+
+    public static final Item COIL;
+    public static final Item ENERGY_MOTOR;
+    public static final Item ENERGY_TOOL_BODY;
+
+    public static final Item MIXED_METAL_INGOT;
+    public static final Item ADVANCED_ALLOY;
+
     static {
         var simpleMaterial = new Item.Settings();
+
         var batteryItem = new BatteryItem(simpleMaterial, 10_000);
-        RECHARGEABLE_BATTERY = register(ItemGroups.INGREDIENTS, Identifiers.of("rechargeable_battery"), batteryItem);
+        RECHARGEABLE_BATTERY = register(ItemGroups.INGREDIENTS,
+                Identifiers.of("rechargeable_battery"), batteryItem);
+
+        var advancedBatteryItem = new BatteryItem(simpleMaterial, 100_000);
+        ADVANCED_RECHARGEABLE_BATTERY = register(ItemGroups.INGREDIENTS,
+                Identifiers.of("advanced_rechargeable_battery"), advancedBatteryItem);
 
         ENERGY_CRYSTAL = register(ItemGroups.INGREDIENTS, Identifiers.of("energy_crystal"), simpleMaterial);
         ENERGY_CIRCUIT = register(ItemGroups.INGREDIENTS, Identifiers.of("energy_circuit"), simpleMaterial);
-        ADVANCED_ENERGY_CIRCUIT = register(ItemGroups.INGREDIENTS, Identifiers.of("advanced_energy_circuit"), simpleMaterial);
+        ADVANCED_ENERGY_CIRCUIT = register(ItemGroups.INGREDIENTS,
+                Identifiers.of("advanced_energy_circuit"), simpleMaterial);
+
+        COIL = register(ItemGroups.INGREDIENTS, Identifiers.of("coil"), simpleMaterial);
+        ENERGY_MOTOR = register(ItemGroups.INGREDIENTS, Identifiers.of("energy_motor"), simpleMaterial);
+
+        var energyToolBody = new BatteryItem(simpleMaterial, 30_000);
+        ENERGY_TOOL_BODY = register(ItemGroups.INGREDIENTS, Identifiers.of("energy_tool_body"), energyToolBody);
+        MIXED_METAL_INGOT = register(ItemGroups.INGREDIENTS, Identifiers.of("mixed_metal_ingot"), simpleMaterial);
+        ADVANCED_ALLOY = register(ItemGroups.INGREDIENTS, Identifiers.of("advanced_alloy"), simpleMaterial);
     }
 }
